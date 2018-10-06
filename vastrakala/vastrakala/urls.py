f8b342e2e9870d741817e23ab1aaf4338e367cc8
Amitha M. D <amithamd@gmail.com>
2018-10-06 21:06:09 +0530

    context processor media added

--- vastrakala/vastrakala/urls.py
+++ vastrakala/vastrakala/urls.py
@@ -21,6 +21,8 @@ from django.conf.urls.static import static
 
 from django.contrib.auth import views as auth_views
 
+# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
